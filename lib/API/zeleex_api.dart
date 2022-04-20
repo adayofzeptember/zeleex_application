b50688353048e4e6f9a24cc0dbd5d2be60982e79
Your Name <chawanthon.wira@gmail.com>
2022-04-20 12:53:11 +0700

    api

---
+++ lib/API/zeleex_api.dart
@@ -0,0 +1,71 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'package:zeleex_application/API/zeleex_api_model.dart';
+
+import '../Plate.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late Future<List<Data2>> futureData;
+  @override
+  void initState() {
+    super.initState();
+    futureData = fetch_HelpCenter();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'Kanit',
+        primarySwatch: Palette.kToDark,
+        appBarTheme: AppBarTheme(color: Palette.kToDark),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          systemOverlayStyle: SystemUiOverlayStyle(
+            statusBarIconBrightness: Brightness.light,
+            statusBarBrightness: Brightness.light,
+            statusBarColor: Palette.kToDark,
+          ),
+          backgroundColor: Palette.kToDark,
+          title: Text(
+            "on API ",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        body: Center(
+          child: FutureBuilder<List<Data2>>(
+            future: futureData,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                List<Data2>? data = snapshot.data;
+                return ListView.builder(
+                    itemCount: data?.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        child: Text(data![index].title.toString()),
+                      );
+                    });
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+              // By default show a loading spinner.
+              return CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
